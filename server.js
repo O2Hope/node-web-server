0406390d92692d03e1c819a30a0cccd46ce17748
Agustin Ruiz <agustin.ruiz@ey.com>
2018-12-08 23:11:40 -0600

    Setup for heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const hbs = require('hbs');
 const fs = require('fs');
 var app = express();
 
+const port = process.env.PORT || 3000;
 ///Config partials and view engine
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
@@ -19,11 +20,12 @@ app.use((req, res, next) => {
   })
   next();
 });
-app.use((req, res, next ) =>{
-  res.render('maintenance.hbs',{
-    pageTitle: 'Under Maintenance'
-  })
-})
+
+// app.use((req, res, next ) =>{
+//   res.render('maintenance.hbs',{
+//     pageTitle: 'Under Maintenance'
+//   })
+// })
 
 ///Helpers
 hbs.registerHelper('getCurrentYear', () =>{
@@ -56,6 +58,6 @@ app.get('/bad',(req, res) => {
 })
 
 
-app.listen(3000, () => {
-  console.log('server is up on port 3000')
+app.listen(port, () => {
+  console.log(`server is up on port ${port}`)
 });
